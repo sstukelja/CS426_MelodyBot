9c4e0a5e6d422f7149c8f0efe442b2a4d7fe64ec
sstukelja <sstukelja@nevada.unr.edu>
2018-01-29 21:18:50 -0800

    Frame rework

--- PythonGUI/tkintertest.py
+++ PythonGUI/tkintertest.py
@@ -5,6 +5,7 @@ from tkinter import Scale
 from tkinter import Frame
 from tkinter import IntVar
 from tkinter import Entry
+from tkinter import Radiobutton
 import pygame
 
 #Import functions
@@ -38,15 +39,25 @@ window = tk.Tk()
 
 window.title("MelodyBot")
 window.geometry("1200x600")
-window.configure(background="white")
+window.configure(background="#e2faff")
 
 pygame.mixer.init()
+
+# Frames
+frame_options = Frame(master=window, width=300, height=500)
+frame_options.grid(row=0, column=1)
+
+frame_media = Frame(master=window, width=100, height=100, bg="white", padx=100)
+frame_media.grid(row=0, column=0)
+
+frame_radio = Frame(master=frame_options, width=200, height=50)
+frame_radio.grid(row=1, column=0)
 
 # List of tuples containing file information
 list_songs = list()
 
 # Listbox containing user-facing song names
-listbox_songs = tk.Listbox(master=window, width=50, selectmode=tk.BROWSE)
+listbox_songs = tk.Listbox(master=frame_media, width=50, selectmode=tk.BROWSE)
 listbox_songs.grid(row=0, column=0)
 
 # Menus
@@ -55,31 +66,34 @@ window.configure(menu=menu_bar)
 
 menu_file = Menu(menu_bar)
 menu_edit = Menu(menu_bar)
+menu_help = Menu(menu_bar)
 
 menu_file.add_command(label="Import Song...",command=add_song)
+menu_file.add_separator()
+menu_file.add_command(label="Exit...")
 menu_edit.add_command(label="Choose Settings...")
+menu_help.add_command(label="About...")
 
 menu_bar.add_cascade(label="File", menu=menu_file)
 menu_bar.add_cascade(label="Edit", menu=menu_edit)
+menu_bar.add_cascade(label="Help", menu=menu_help)
 
-frame_options = Frame(master=window, width=200, height=100)
-frame_options.grid(row=0, column=1)
 # Buttons controlling music playback
 
 #Play button
 load_image_play = tk.PhotoImage(file="C:/Users/Lepi/Desktop/CS425/PythonGUI/Play.png")
-button_play = tk.Button(master=window, image=load_image_play, command=play_song)
+button_play = tk.Button(master=frame_media, image=load_image_play, command=play_song)
 button_play["bg"] = "white"
 button_play["border"] = "0"
 button_play.grid(row=1, column=0)
 
 #Stop button
 load_image_stop = tk.PhotoImage(file="C:/Users/Lepi/Desktop/CS425/PythonGUI/Pause.png")
-button_stop = tk.Button(master=window, image=load_image_stop, command=pause_song)
+button_stop = tk.Button(master=frame_media, image=load_image_stop, command=pause_song)
 button_stop["bg"] = "white"
 button_stop["border"] = "0"
 
-# Generation Options
+# Generation Options - Time Slider
 slider_time = Scale(master=frame_options, bg="white", label="Song Length (seconds)", to=600, orient=tk.HORIZONTAL, length=300)
 slider_time.grid(row=0, column=0)
 
@@ -89,6 +103,13 @@ time_entry.grid(row=0, column=1)
 
 time_button = tk.Button(master=frame_options, bg="white", text="Set", height=1, width=3, command=set_time)
 time_button.grid(row=0, column=2)
+
+# Generation Options - Instrument Selection
+radio_options = IntVar()
+
+Radiobutton(master=frame_radio, text="Piano", variable=radio_options, value=1).grid(row=0, column=0, sticky=tk.W)
+Radiobutton(master=frame_radio, text="Other", variable=radio_options, value=2).grid(row=0, column=1, sticky=tk.W)
+Radiobutton(master=frame_radio, text="Other2", variable=radio_options, value=3).grid(row=0, column=2, sticky=tk.W)
 
 # Window application loop
 window.mainloop()
